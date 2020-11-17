12970b4d0451517ed828254bb4b98d8807b11161
zhangsq <zhangsq@mapbar.com>
2020-11-17 14:00:42 +0800

    fix: 修正错误

--- index.js
+++ index.js
@@ -1166,10 +1166,11 @@ const exec = __webpack_require__(514);
 
 (async function() {
     await exec.exec("ls");
-    await exec.exec("sudo apt-get update");
-    await exec.exec("sudo apt-get install wget -y");
+    // await exec.exec("sudo apt-get update");
+    // await exec.exec("sudo apt-get install wget -y");
     await exec.exec("wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip");
     await exec.exec("unzip build-wrapper-linux-x86.zip");
+    await exec.exex("ls -lh");
     await exec.exec("mv build-wrapper-linux-x86/* /usr/local/bin/");
     await exec.extc("build-wrapper-linux-x86")
 })()
